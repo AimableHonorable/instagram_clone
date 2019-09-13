8236bf6ce011434c82939fe5619d6859fe0cfb7a
Aimable <aimatwiri@gmail.com>
2019-09-13 10:57:00 +0200

    prohibit the user to post empty content

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+
+  validate :any_present?
+
+  def any_present?
+    if %w(texts image).all?{|attr| self[attr].blank?}
+      errors.add :base, "please post at least one field"
+    end
+  end
 end
